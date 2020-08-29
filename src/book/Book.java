7042e97c9b94b1b9f131dd727de253b78749adc1
vinay <vinay.yadav_cs19@gla.ac.in>
2020-08-29 15:26:56 +0530

    Create a private field for class Book.java;

--- src/book/Book.java
+++ src/book/Book.java
@@ -1,4 +1,19 @@
 package book;
 
 public class Book {
+    private int isbn;
+    private String bookName;
+    private String authorName;
+
+    public String getBookName() {
+        return bookName;
+    }
+    public String getAuthorName() {
+        return bookName;
+    }
+    public int getIsbn() {
+        return isbn;
+    }
 }
+
+
