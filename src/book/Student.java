3d1e1e1df510c0353fcb3e9a9e992bdc1affa515
vinay <vinay.yadav_cs19@gla.ac.in>
2020-08-29 17:16:35 +0530

    create the getter and setter method for class Student.java

--- src/book/Student.java
+++ src/book/Student.java
@@ -1,10 +1,36 @@
 package book;
 
 public class Student {
-    private String firstNameOfStudent;
-    private String middleNameOfStudent;
-    private String lastNameOfStudent;
+    private String studentName;
     private long universityRollNo;
-    private int noOfBookStudentIssued;
+    private String nameOfBooksStudentIssued[] = new String[] {};
+    private int noOfBooksStudentIssued;
 
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentName() {
+        this.studentName = studentName;
+    }
+    public long getUniversityRollNo() {
+        return  universityRollNo;
+    }
+
+    public void setUniversityRollNo() {
+        this.universityRollNo = universityRollNo;
+    }
+
+    public int getNoOfBookStudentIssued() {
+         return noOfBooksStudentIssued;
+    }
+    public void setNoOfBooksStudentIssued() {
+    this.nameOfBooksStudentIssued = nameOfBooksStudentIssued;
+    }
+    public String[] getNameOfBooksStudentIssued() {
+        return nameOfBooksStudentIssued;
+    }
+    public void setNameOfBooksStudentIssued(String[] nameOfBooksStudentIssued) {
+        this.nameOfBooksStudentIssued = nameOfBooksStudentIssued;
+    }
 }
+
