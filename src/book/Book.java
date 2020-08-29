62649aa19bb02ba7b22caad92d0e2cbbe88966f9
vinay <vinay.yadav_cs19@gla.ac.in>
2020-08-29 16:04:38 +0530

    Create constructor method for class Book.java;

--- src/book/Book.java
+++ src/book/Book.java
@@ -1,5 +1,7 @@
 package book;
 
+import java.util.Objects;
+
 public class Book {
     private int isbn;
     private String bookName;
@@ -8,17 +10,56 @@ public class Book {
     public String getBookName() {
         return bookName;
     }
-    public void setBookName() {
+    public void setBookName(String bookName) {
         this.bookName = bookName;
     }
     public String getAuthorName() {
         return bookName;
     }
-
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
     public int getIsbn() {
         return isbn;
     }
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
 
+    public Book(String authorName) {
+        this.authorName = authorName;
+    }
+    public Book(String authorName, String bookName, int isbn) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn=" + isbn +
+                ", bookName='" + bookName + '\'' +
+                ", authorName='" + authorName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return getIsbn() == book.getIsbn() &&
+                Objects.equals(getBookName(), book.getBookName()) &&
+                Objects.equals(getAuthorName(), book.getAuthorName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIsbn(), getBookName(), getAuthorName());
+    }
 }
+
+
 
 
